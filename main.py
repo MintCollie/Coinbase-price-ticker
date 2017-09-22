05736ea75ec90e7c37d971dbdd053c167dd6d01b
Mint <dragoncool57@yahoo.com>
2017-09-22 15:46:15 -0400

    increased time

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import time
 url = 'https://api.coinbase.com/v2/prices/USD/spot?'
 req = requests.get(url)
 data = req.json()
-bb = (data['data'][0]['amount'])
+bit = (data['data'][0]['amount'])
 while True:
-    print "1 BTC = %s USD" % bb
-    time.sleep(2)
+    print "1 BTC = %s USD" % bit
+    time.sleep(60)
+
+
