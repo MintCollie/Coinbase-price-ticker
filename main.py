32cfc238d2be5ee72421ff3e0542bdc5ba8bf09f
Mint Collie <dragoncool57@yahoo.com>
2017-10-01 18:46:59 -0400

    added shebang

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import requests
 import time
 test = 'https://api.nicehash.com/api?method=stats.provider.ex&addr=37sCnRwMW7w8V7Y4zyVZD5uCmc9N1kZ2Q8&callback=jQuery111304118770088094035_1506738346881&_=1506738346882'
@@ -8,5 +9,3 @@ bit = (data['data'][0]['amount'])
 while True:
     print("1 BTC = %s USD" % bit)
     time.sleep(60)
-
-
