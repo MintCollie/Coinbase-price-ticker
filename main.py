deed08b7ad872c4862e221fc4f110db7f215591d
Mint <dragoncool57@yahoo.com>
2017-10-02 18:20:23 -0400

    pos guy update

--- main.py
+++ main.py
@@ -3,21 +3,45 @@
 import requests
 import time
 from tkinter import *
+import random
 
 # variables
 test = 'https://api.nicehash.com/api?method=stats.provider.ex&addr=37sCnRwMW7w8V7Y4zyVZD5uCmc9N1kZ2Q8&callback=jQuery111304118770088094035_1506738346881&_=1506738346882'
 url = 'https://api.coinbase.com/v2/prices/USD/spot?'
-req = requests.get(url)
-data = req.json()
-bit = (data['data'][0]['amount'])
+
+
+
+# def function to update
+def update_bitcoin_ui():
+
+    # update the data sourced form the website
+    req = requests.get(url)
+    data = req.json()
+    bit = (data['data'][0]['amount'])
+
+    # update the gui to reflect new value
+    thelabel.config(text = "1 BTC = %s USD" % bit)
+
+    # verify the Ui is updating
+    #thelabel.config(text=str(random.random()))
+    root.after(1000, update_bitcoin_ui)
 
 # gui workspace
 root = Tk()
-thelabel = Label(root, text = bit)
+thelabel = Label(root, text = "")
+
+# set more of the gui and launch the ui
 thelabel.pack()
+root.after(1000, update_bitcoin_ui)
 root.mainloop()
 
+
+
 # loop for printing price
+"""
 while True:
+
+    bit_string_var.set("1 BTC = %s USD" % bit)
     print("1 BTC = %s USD" % bit)
-    time.sleep(60)
+    time.sleep(5)
+"""
